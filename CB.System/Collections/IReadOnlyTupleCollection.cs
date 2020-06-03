27fad35f4c07416ae25d05242ffafcd0cae363ea
Christian Borck <borck@hotmail.de>
2020-06-03 23:21:25 +0200

    [UPDATE] making unnecessary interface obsolete

--- CB.System/Collections/IReadOnlyTupleCollection.cs
+++ CB.System/Collections/IReadOnlyTupleCollection.cs
@@ -4,5 +4,6 @@ using System.Collections.Generic;
 
 
 namespace CB.System.Collections {
+  [Obsolete( "Use System.Collections.Generic.IReadOnlyCollection<System.Tuple<T1,T2>> instead" )]
   public interface IReadOnlyTupleCollection<TFirst, TSecond> : IReadOnlyCollection<Tuple<TFirst, TSecond>> { }
 }
