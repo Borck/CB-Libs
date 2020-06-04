86204ce5f421066cd988dae398a052aaebbd54c8
Christian Borck <borck@hotmail.de>
2020-06-04 09:43:51 +0200

    [ADDED] missing classes in CB.System

---
+++ CB.System/Diagnostics/ProcessInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+
+
+namespace CB.Util.System.Win {
+  public class ProcessInfo {
+    public enum States {
+      Running,
+      Ready,
+      Unknown
+    };
+
+
+
+    private readonly Process _proc;
+
+
+
+    public ProcessInfo(int processId, string commandline) {
+      _proc = Process.GetProcessById(processId);
+      CommandLine = commandline;
+    }
+
+
+
+    public string CommandLine { get; }
+
+    public States State {
+      get {
+        try {
+          return _proc.Responding ? States.Ready : States.Running;
+        } catch (Exception) {
+          return States.Unknown;
+        }
+      }
+    }
+  }
+}
---
+++ CB.System/TypeX.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace CB.System {
+  public static class TypeX {
+    public static bool TryGetCustomAttribute<TAttribute>(
+      this Type type,
+      bool inherit,
+      out TAttribute attribute)
+      where TAttribute : Attribute {
+      attribute = type.GetCustomAttributes(
+                        typeof(TAttribute),
+                        inherit
+                      )
+                      .FirstOrDefault() as TAttribute;
+      return attribute != null;
+    }
+  }
+}
