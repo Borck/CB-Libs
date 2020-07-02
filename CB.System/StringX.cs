8277afee5fa93815b39a776389aa6bd66b944b32
Christian Borck <borck@hotmail.de>
2020-07-02 14:50:05 +0200

    [ADDED]  some string extensions for separating/parsing

--- CB.System/StringX.cs
+++ CB.System/StringX.cs
@@ -1,29 +1,30 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 
 
 
 namespace CB.System {
   public static class StringX {
     public static (string left, string right) Separate([NotNull] this string @string, params char[] separators) {
-      return @string.Separate( @string.IndexOfAny( separators ) );
+      return @string.Separate(@string.IndexOfAny(separators));
     }
 
 
 
     public static (string left, string right) Separate([NotNull] this string @string, char separator) {
-      return @string.Separate( @string.IndexOf( separator ) );
+      return @string.Separate(@string.IndexOf(separator));
     }
 
 
 
     public static (string left, string right) SeparateLast([NotNull] this string @string, params char[] separators) {
-      return @string.Separate( @string.LastIndexOfAny( separators ) );
+      return @string.Separate(@string.LastIndexOfAny(separators));
     }
 
 
 
     public static (string left, string right) SeparateLast([NotNull] this string @string, char separator) {
-      return @string.Separate( @string.LastIndexOf( separator ) );
+      return @string.Separate(@string.LastIndexOf(separator));
     }
 
 
@@ -31,55 +32,98 @@ namespace CB.System {
     private static (string left, string right) Separate([NotNull] this string @string, int indexOfSeparator) {
       switch (indexOfSeparator) {
         case -1:
-          return ( @string, default(string) );
+          return (@string, default);
         case 0:
-          return ( string.Empty, @string );
+          return (string.Empty, @string);
         default:
           return (
-                   @string.Substring( 0, indexOfSeparator ),
+                   @string.Substring(0, indexOfSeparator),
                    @string.Length != indexOfSeparator + 1
-                     ? @string.Substring( indexOfSeparator + 1 )
-                     : string.Empty );
+                     ? @string.Substring(indexOfSeparator + 1)
+                     : string.Empty);
       }
     }
 
 
 
-    public static bool TryHead(this string @string, char[] separators, out string head, out string tail) {
-      var strings = @string.Split( separators, 2 );
+    public static bool TrySeparate(
+      this string @string,
+      char[] separators,
+      out string first,
+      out string rest) {
+      var strings = @string.Split(separators, 2);
       if (strings.Length == 2) {
-        head = strings[0];
-        tail = strings[1];
+        first = strings[0];
+        rest = strings[1];
         return true;
       }
 
-      head = @string;
-      tail = default(string);
+      first = @string;
+      rest = default;
       return false;
     }
 
 
 
-    public static bool TryTail([NotNull] this string @string, char separator, out string head, out string tail) {
-      var idx = @string.LastIndexOf( separator );
+    public static bool TrySeparate<T>(this string @string,
+                                      char[] separators,
+                                      out T first,
+                                      out string rest,
+                                      Func<string, T> parseFirst) {
+      var success = TrySeparate(@string, separators, out var firstString, out rest);
+      first = success ? parseFirst(firstString) : default;
+      return success;
+    }
+
+
+
+    public static bool TrySeparateLast([NotNull] this string @string,
+                                       char separator,
+                                       out string rest,
+                                       out string last) {
+      var idx = @string.LastIndexOf(separator);
       switch (idx) {
         case -1:
-          head = @string;
-          tail = default(string);
+          rest = @string;
+          last = default;
           return false;
         case 0:
-          head = "";
-          tail = idx + 1 < @string.Length
-                   ? @string.Substring( idx + 1 )
+          rest = "";
+          last = idx + 1 < @string.Length
+                   ? @string.Substring(idx + 1)
                    : "";
           return true;
         default:
-          head = @string.Substring( 0, idx );
-          tail = idx + 1 < @string.Length
-                   ? @string.Substring( idx + 1 )
+          rest = @string.Substring(0, idx);
+          last = idx + 1 < @string.Length
+                   ? @string.Substring(idx + 1)
                    : "";
           return true;
       }
     }
+
+
+
+    public static bool TrySeparateLast<T>([NotNull] this string @string,
+                                          char separator,
+                                          out string rest,
+                                          out T last,
+                                          Func<string, T> parseLast) {
+      var success = TrySeparateLast(@string, separator, out var lastString, out rest);
+      last = success ? parseLast(lastString) : default;
+      return success;
+    }
+
+
+
+    [Obsolete("Use TrySeparate instead")]
+    public static bool TryHead(this string @string, char[] separators, out string head, out string tail)
+      => TrySeparate(@string, separators, out head, out tail);
+
+
+
+    [Obsolete("Use TrySeparateLast instead")]
+    public static bool TryTail([NotNull] this string @string, char separator, out string head, out string tail)
+      => TrySeparateLast(@string, separator, out head, out tail);
   }
 }
