a6c3f5abe75845e023a40c663bb35df79f14e392
Christian Borck <borck@hotmail.de>
2020-07-02 14:55:07 +0200

    [ADDED] extension to create System.Windows.Media.Imaging.BitmapSource from System.Drawing.Bitmap

---
+++ CB.WPF/Media/BitmapX.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+
+
+namespace CB.WPF.Media {
+  public static class BitmapX {
+    [DllImport("gdi32.dll")]
+    private static extern bool DeleteObject(IntPtr hObject);
+
+
+
+    /// <summary>
+    ///   Creates a <see cref="System.Windows.Media.Imaging.BitmapSource" /> from a <see cref="System.Drawing.Bitmap" />
+    /// </summary>
+    /// <param name="bitmap"></param>
+    /// <returns></returns>
+    public static BitmapSource CreateBitmapSource([NotNull] this Bitmap bitmap) {
+      var hBitmap = bitmap.GetHbitmap();
+      try {
+        return Imaging.CreateBitmapSourceFromHBitmap(
+          hBitmap,
+          IntPtr.Zero,
+          Int32Rect.Empty,
+          BitmapSizeOptions.FromEmptyOptions()
+        );
+      } finally {
+        DeleteObject(hBitmap);
+      }
+    }
+  }
+}
