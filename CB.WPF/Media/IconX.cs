8c6d28308c29a2be8b3544b24d1a79bcecec471c
Christian Borck <borck@hotmail.de>
2020-07-04 00:46:53 +0200

    [ADDED] missing deprecation attribute

--- CB.WPF/Media/IconX.cs
+++ CB.WPF/Media/IconX.cs
@@ -1,9 +1,11 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Media;
 
 
 
 namespace CB.WPF.Media {
+  [Obsolete("Use CB.WPF.Drawing.IconX instead")]
   public static class IconX {
     public static ImageSource ToImageSource(this Icon icon) => Drawing.IconX.ToImageSource(icon);
   }
