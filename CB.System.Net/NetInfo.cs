efc18fbe41ba6538eb63bf0596ee1efe51fdd272
Christian Borck <christian.borck@b-tu.de>
2021-01-01 19:10:04 +0100

    [ADDED] async alternative methods to NetInfo

--- CB.System.Net/NetInfo.cs
+++ CB.System.Net/NetInfo.cs
@@ -3,14 +3,30 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 
 
 
 namespace CB.System.Net {
   public static class NetInfo {
+    /// <summary>
+    ///   Get all ip addresses of this machine
+    /// </summary>
+    /// <returns></returns>
     public static IPAddress[] GetLocalIpAddresses() {
       var strHostName = Dns.GetHostName();
       return Dns.GetHostAddresses(strHostName);
+    }
+
+
+
+    /// <summary>
+    ///   Get all ip addresses of this machine asynchronous
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<IPAddress[]> GetLocalIpAddressesAsync() {
+      var strHostName = Dns.GetHostName();
+      return await Dns.GetHostAddressesAsync(strHostName);
     }
 
 
@@ -68,9 +84,52 @@ namespace CB.System.Net {
 
 
 
+    public static async Task<PhysicalAddress?> GetPhysicalAddressAsync(IPAddress ipAddress) {
+      var arpStream = Execute("arp", "-a " + ipAddress);
+
+      // Consume first three lines
+      await arpStream.ReadLineAsync();
+      await arpStream.ReadLineAsync();
+      await arpStream.ReadLineAsync();
+
+      // Read entries
+      while (!arpStream.EndOfStream) {
+        var line = await arpStream.ReadLineAsync();
+        var tokens = line?.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+        if (tokens?.Length != 3) {
+          continue;
+        }
+
+        var mac = tokens[1];
+        return PhysicalAddress.Parse(mac);
+      }
+
+      return default;
+    }
+
+
+
+    /// <summary>
+    ///   Get the host name of a ip address
+    /// </summary>
+    /// <param name="ipAddress"></param>
+    /// <returns></returns>
     public static string GetHostName(IPAddress ipAddress)
       => Dns
          .GetHostEntry(ipAddress)
          .HostName;
+
+
+
+    /// <summary>
+    ///   Get the host name of a ip address asynchronous
+    /// </summary>
+    /// <param name="ipAddress"></param>
+    /// <returns></returns>
+    public static async Task<string> GetHostNameAsync(IPAddress ipAddress)
+      => (await Dns
+            .GetHostEntryAsync(ipAddress))
+        .HostName;
   }
 }
