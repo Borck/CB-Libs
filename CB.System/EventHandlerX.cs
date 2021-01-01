5e6376ca934b43613e190fcd82249e248736489c
Christian Borck <christian.borck@b-tu.de>
2021-01-01 19:32:41 +0100

    [ADDED] extension EventHandler<T>.InvokeAsync

--- CB.System/EventHandlerX.cs
+++ CB.System/EventHandlerX.cs
@@ -6,17 +6,41 @@ using System.Threading.Tasks;
 
 namespace CB.System {
   public static class EventHandlerX {
-    private static readonly Action[] _noActions = Array.Empty<Action>();
+    private static readonly Action[] NoActions = Array.Empty<Action>();
 
 
 
+    /// <summary>
+    ///   Invokes each invocation, possibly in parallel
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="eventHandler"></param>
+    /// <param name="arg"></param>
     public static void InvokeParallel<T>(this EventHandler<T> eventHandler, T arg) {
       var actions = eventHandler
                     ?.GetInvocationList()
-                    .Select( del => new Action( () => del.DynamicInvoke( arg ) ) )
+                    .Select(del => new Action(() => del.DynamicInvoke(arg)))
                     .ToArray() ??
-                    _noActions;
-      Parallel.Invoke( actions );
+                    NoActions;
+      Parallel.Invoke(actions);
+    }
+
+
+
+    /// <summary>
+    ///   Subscribes an EventHandle and invokes that delegate asynchronous.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="eventHandler"></param>
+    /// <param name="sender"></param>
+    /// <param name="arg"></param>
+    /// <returns></returns>
+    public static Task InvokeAsync<T>(this EventHandler<T> eventHandler, object sender, T arg) {
+      return Task.Factory.FromAsync(
+        (asyncCallback, @object) => eventHandler.BeginInvoke(sender, arg, asyncCallback, @object),
+        eventHandler.EndInvoke,
+        null
+      );
     }
   }
 }
