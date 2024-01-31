fb161a486e95ab6c31aecb7cdbb6bdf0b0e38b40
Christian Borck <borck@hotmail.de>
2024-01-31 12:37:38 +0100

    [FIXED] IpEndPointX.TryParse doesnt take port range under consideration

--- CB.System.Net/IpEndPointX.cs
+++ CB.System.Net/IpEndPointX.cs
@@ -17,34 +17,29 @@ namespace CB.System.Net {
     /// <param name="string"></param>
     /// <returns></returns>
     public static IPEndPoint ParseIpEndPoint(string @string) {
-      if (!@string.TrySeparateLast( PORT_SEPARATOR, out var addressStr, out var portStr )) {
-        throw new FormatException( "Invalid IP endpoint format" );
-      }
-
-      if (!int.TryParse( portStr, out var port )) {
-        throw new FormatException( "Invalid IP port format" );
-      }
-
-      if (!IPAddress.TryParse( addressStr, out var address )) {
-        throw new FormatException( "Invalid IP address format" );
-      }
-
-      return new IPEndPoint( address, port );
+      return !@string.TrySeparateLast(PORT_SEPARATOR, out var addressStr, out var portStr)
+        ? throw new FormatException("Invalid IP endpoint format")
+        : !int.TryParse(portStr, out var port)
+        ? throw new FormatException("Invalid IP port format")
+        : !IPAddress.TryParse(addressStr, out var address)
+        ? throw new FormatException("Invalid IP address format")
+        : new IPEndPoint(address, port);
     }
 
 
 
     /// <summary>
-    ///   Tries to parses IPv4 and IPv6 notation.
+    ///   Tries to parse IPv4 and IPv6 notation.
     /// </summary>
     /// <param name="string"></param>
     /// <param name="endPoint">the output IPEndPoint</param>
     /// <returns>true if parseable, otherwise false</returns>
-    public static bool TryParse(string @string, out IPEndPoint endPoint) {
-      if (@string.TrySeparateLast( PORT_SEPARATOR, out var addressStr, out var portStr ) &&
-          int.TryParse( portStr, out var port ) &&
-          IPAddress.TryParse( addressStr, out var address )) {
-        endPoint = new IPEndPoint( address, port );
+    public static bool TryParse(string @string, out IPEndPoint? endPoint) {
+      if (@string.TrySeparateLast(PORT_SEPARATOR, out var addressStr, out var portStr) &&
+        int.TryParse(portStr, out var port) &&
+        port >= 0 && port <= 65535 &&
+        IPAddress.TryParse(addressStr, out var address)) {
+        endPoint = new IPEndPoint(address, port);
         return true;
       }
 
@@ -55,16 +50,15 @@ namespace CB.System.Net {
 
 
     private static int ParsePort(IReadOnlyList<string> endPointTokens) {
-      if (int.TryParse(
+      return int.TryParse(
         endPointTokens[endPointTokens.Count - 1],
         NumberStyles.None,
         NumberFormatInfo.CurrentInfo,
         out var port
-      )) {
-        return port;
-      }
-
-      throw new FormatException( "Invalid port" );
+      )
+        ? port
+        :
+      throw new FormatException("Invalid port");
     }
   }
 }
